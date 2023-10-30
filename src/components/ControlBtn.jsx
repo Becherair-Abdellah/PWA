d9b24e1ef7a4116d597398c0f38eff1ba05a96d3
Abdellah <abdeallahdz2004@gmail.com>
2023-10-30 09:14:21 +0100

    reset last changes

---
+++ src/components/ControlBtn.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function ControlBtn({icon}) {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default ControlBtn
--- src/components/LiItem.jsx
+++ src/components/LiItem.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 import {BiChevronDown} from 'react-icons/bi'
 function LiItem() {
   return (
-    <li className='flex items-center justify-between  rounded-[5px]  border-[#424242] border-[1.8px] text-[15px] w-full  px-3 text-white'>
-      <div className='flex items-start flex-col'>
-        <span className='font-semibold text-[14px]'>Choco Land</span>
+    <li className='flex items-center justify-between  rounded-[5px]  border-[#424242] border-[1.8px] text-[15px] w-full p-2 px-3 text-white'>
+      <div className='flex items-start flex-col gap-1'>
+        <span className='font-semibold text-[16px]'>Choco Land</span>
         <span className='text-[12px] text-primaryText'>08-09-2023 at 23:20:05</span>
       </div>
       <div className='flex items-center'>
@@ -12,7 +12,7 @@ function LiItem() {
             60 <br/>
             <span className='text-[12px] font-normal text-primaryText'>7 items</span>
         </span>
-        <span className=' rounded-sm bg-icon ml-3'>
+        <span className=' rounded-sm bg-icon ml-5'>
             <BiChevronDown size={20} color='#594EF1' className='font-bold' />
         </span>
       </div>
